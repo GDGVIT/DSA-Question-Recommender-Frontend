2a468696bdefaa01b2ee966a1d9d6693bd5957d0
Rounak Agrawal <rounak1430@gmail.com>
2023-12-27 11:46:43 +0545

    fix: linting issues fixed

--- dsa-question-recommender/src/app/auth/layout.js
+++ dsa-question-recommender/src/app/auth/layout.js
@@ -1,13 +1,11 @@
-// import React from 'react';
-
 const DashboardLayout = ({ children }) => {
   return (
-    <main className="flex items-center justify-center min-h-screen bg-auth bg-cover">
-      <div className="w-full max-w-lg bg-[#082032] bg-opacity-80 py-10 px-10 rounded-md">
+    <main className='flex items-center justify-center min-h-screen bg-auth bg-cover'>
+      <div className='w-full max-w-lg bg-[#082032] bg-opacity-80 py-10 px-10 rounded-md'>
         {children}
       </div>
     </main>
-  );
-};
+  )
+}
 
-export default DashboardLayout;
+export default DashboardLayout
