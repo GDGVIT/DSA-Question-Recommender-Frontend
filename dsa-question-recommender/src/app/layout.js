24f540325a2178e683f26d3b2c5c33997bf7430d
Rounak Agrawal <rounak1430@gmail.com>
2023-12-27 11:46:43 +0545

    fix: linting issues fixed

--- dsa-question-recommender/src/app/layout.js
+++ dsa-question-recommender/src/app/layout.js
@@ -2,16 +2,16 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 
 // const inter = Inter({ subsets: ['latin'] })
-const poppins = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], })
+const poppins = Poppins({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] })
 
 export const metadata = {
   title: 'DSA Question Recommender',
-  description: 'DSA Question Recommender using ml',
+  description: 'DSA Question Recommender using ml'
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout ({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={poppins.className}>{children}</body>
     </html>
   )
